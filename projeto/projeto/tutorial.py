71d43c5a11122cab837a58d165b238d355dcbe9e
Deividy Samuel <Deividysamuel2022@gmail.com>
2022-05-22 19:59:49 -0300

    django

---
+++ projeto/projeto/tutorial.py
@@ -0,0 +1,17 @@
+# primeiro instala o django: pip install django
+
+# segundo cria a pasta principal do seu projeto: django-admin startproject projeto .
+
+# inicia o servidor local: python manage.py runserver
+
+# fecha o servidor local: ctrl c
+
+# cria pasta app do seu projeto: python manage.py startapp blog
+
+
+
+
+
+
+
+
